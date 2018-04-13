21760d5804bdcd7120a4afd6fbfcab5b31686a1d
Liuwei@Softwarefactory <mail@liuweizzuie.com>
2018-04-13 15:19:52 +0800

    DelegateSingleThreadEngine

---
+++ Miae.Common/Threading/Engine/DelegateSingleThreadEngine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Miae.Threading.Engine
+{
+    public class DelegateSingleThreadEngine : BaseSingleThreadEngine
+    {
+        private Func<bool> task;
+
+        /// <summary>
+        /// 获取 DelegateSingleThreadEngine 的新实例。
+        /// </summary>
+        /// <param name="task">委托的任务。</param>
+        public DelegateSingleThreadEngine(Func<bool> task)
+        {
+            this.task = task;
+        }
+
+        protected override bool EngineTask()
+        {
+            return task();
+        }
+    }
+}
